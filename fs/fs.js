6b0f4e02ef82116b6c363a57f8a46d7b38dd5e2f
liuyidi <928171466@qq.com>
2019-04-18 23:56:07 +0800

    init

---
+++ fs/fs.js
@@ -0,0 +1,56 @@
+var fs = require('fs');
+var data;
+var time;
+
+/***************普通读取***************/ 
+// 同步读取 readFileSync
+var fsSync = () => {
+  try {
+    time = Date.now();
+    data = fs.readFileSync('./file.txt', 'utf8');
+    time = Date.now() - time;
+    console.log('文件内容: \n' + data);
+    console.log('耗时：' + time + 'ms')
+  } catch (err) {
+    console.log('读取文件出错：' + err.message)
+  }
+}
+
+// 异步读取 readFile
+var fsAsync = () => {
+  time = Date.now();
+  fs.readFile('./file.txt', 'utf8', function(err, data) {
+    time = Date.now() - time;
+    if (err) {
+      return console.error('读取文件出错：' + err.message)
+    }
+    console.log('文件内容：' + data)
+    console.log('耗时：' + time + 'ms')
+  })
+}
+
+/***********通过文件流读取createReadStream***************/
+var fsReadStream = () => {
+  var readStream = fs.createReadStream('./file.txt', 'utf8')
+  readStream.on('data', function(chunk) {
+    console.log('读取数据：' + chunk)
+  }).on('error', function(err) {
+    console.log('出错：' + err.message)
+  }).on('end', function() {
+    console.log('没有数据了')
+  }).on('close', function() {
+    console.log('已经关闭')
+  })
+}
+
+/**************文件写入*********** */
+var fsWriteFile = () => {
+  fs.writeFile('./file.txt', 'hello, wolrd', 'utf8', function(err) {
+    if (err) throw err;
+    console.log('文件写入成功')
+  })
+}
+
+
+fsWriteFile();
+fsReadStream();
---
+++ fs/fs.md
@@ -0,0 +1,7 @@
+### Fs
+
+- readFileSync 同步读取
+- readFile 异步读取
+- createReadStream 读取文件流
+- writeFile 文件写入
+- writeFileSync 文件同步写入
